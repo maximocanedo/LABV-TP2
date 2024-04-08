96254cb890237244344af8290ab3425fdf5e90e7
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-04-08 08:20:27 -0300

    Crear tests unitarios closes #2

--- src/frgp/utn/edu/ar/packageTest/EmailTest.java
+++ src/frgp/utn/edu/ar/packageTest/EmailTest.java
@@ -1,14 +1,24 @@
 package frgp.utn.edu.ar.packageTest;
 
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import frgp.utn.edu.ar.entidad.*;
 
 import org.junit.jupiter.api.Test;
 
 class EmailTest {
 
 	@Test
-	void testValidarCorreo() {
-		fail("Not yet implemented");
+	@DisplayName("Validar correos con al menos un número. ")
+	public void testCorreo__AlMenosUnNumero__retornaTrue() {
+		assertTrue(Email.validarCorreo("abcDEFG1"), "No valida correos con al menos un número");
 	}
+	
+	@Test
+	@DisplayName("Validar correos que no tengan números. ")
+	public void testCorreo__AlMenosUnNumero__retornaFalse() {
+		assertFalse(Email.validarCorreo("abcDEFGh"), "No valida correos sin números");
+	}
+	
 
 }
