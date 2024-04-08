08c60148f1ea2f1679eae6dfad53b8072947abff
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-04-08 08:03:31 -0300

    Crear clase y paquete de prueba closes #1

---
+++ src/frgp/utn/edu/ar/packageTest/EmailTest.java
@@ -0,0 +1,14 @@
+package frgp.utn.edu.ar.packageTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmailTest {
+
+	@Test
+	void testValidarCorreo() {
+		fail("Not yet implemented");
+	}
+
+}
