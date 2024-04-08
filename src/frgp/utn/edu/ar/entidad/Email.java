1653e37d6683c02905c699d31ee308bd00ec1982
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-04-08 07:34:48 -0300

    Descargar proyecto

---
+++ src/frgp/utn/edu/ar/entidad/Email.java
@@ -0,0 +1,27 @@
+package frgp.utn.edu.ar.entidad;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email {
+
+	private String correo;
+	
+	public Email(String correo)
+	{
+	   this.correo = correo;
+	}
+
+	public static boolean validarCorreo(String correo) {
+        String regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z0-9]).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(correo);
+        return matcher.matches();
+    }
+	
+	@Override
+	public String toString() {
+		return "Email [correo=" + correo + "]";
+	}
+}
+
